f2e05f6422dff1bb31cf79cc205fa9b2d9ce0f67
Jack Pena <penajackc@gmail.com>
2022-01-11 16:27:38 -0700

    commit tree 6f1f77554b3e294a7736a294af9490707d62f996

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(arr){
+   const sbWin = arr.find(element => element.result === 'W');
+    if(sbWin){
+        return sbWin.year
+    } else {
+        return undefined;
+    }
+}
